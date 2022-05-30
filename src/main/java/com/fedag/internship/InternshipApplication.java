748efd6b961230f90aa76d8274963e60271d7777
fedor <fedorcvetkov7@gmail.com>
2022-05-30 14:03:10 +0300

    Initial commit

---
+++ src/main/java/com/fedag/internship/InternshipApplication.java
@@ -0,0 +1,13 @@
+package com.fedag.internship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternshipApplication.class, args);
+	}
+
+}
