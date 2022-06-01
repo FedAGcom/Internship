074a10728bb44b98d93d44f195f4a8a7277fb4c2
IlyaKayrachka <ilyanur1997@gmail.com>
2022-06-01 20:40:51 +0300

    поправил controller

--- src/main/java/com/fedag/internship/controller/UserController.java
+++ src/main/java/com/fedag/internship/controller/UserController.java
@@ -2,15 +2,15 @@ package com.fedag.internship.controller;
 
 import com.fedag.internship.domain.UserMapperImpl;
 import com.fedag.internship.domain.dto.UserDto;
+import com.fedag.internship.domain.entity.UserEntity;
 import com.fedag.internship.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
 
 
 @RestController
@@ -23,34 +23,28 @@ public class UserController {
 
     @GetMapping("/{id}")
     public ResponseEntity<UserDto> getUser(@PathVariable Long id) {
-        UserDto userDto = Optional.of(id)
-                .map(userService::getUserById)
-                .map(userMapper::toDto)
-                .orElseThrow();
+        UserDto userDto = userMapper.toDto(userService.getUserById(id));
         return new ResponseEntity<>(userDto, HttpStatus.OK);
     }
 
     @GetMapping
-    public ResponseEntity<Page<UserDto>> getAllUsers(@RequestParam int page, @RequestParam int size) {
-        PageRequest pageRequest = PageRequest.of(page, size);
-        Page<UserDto> users = userService.getAllUsers(pageRequest)
+    public ResponseEntity<Page<UserDto>> getAllUsers(@PageableDefault(size = 5, page = 1) Pageable pageable) {
+        Page<UserDto> users = userService.getAllUsers(pageable)
                 .map(userMapper::toDto);
         return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity<?> createUser(@RequestBody UserDto userDto) {
-        Optional.ofNullable(userDto)
-                .map(userMapper::toEntity)
-                .map(userService::addUser);
+        UserEntity userEntity = userMapper.toEntity(userDto);
+        userService.addUser(userEntity);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @PutMapping
     public ResponseEntity<?> updateUser(@RequestBody UserDto userDto) {
-        Optional.ofNullable(userDto)
-                .map(userMapper::toEntity)
-                .map(userService::editUser);
+        UserEntity userEntity = userMapper.toEntity(userDto);
+        userService.editUser(userEntity);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
