2ac3322a88bbf578e18a97ee25045c3eee75fcdf
Vasiliy <Kiriluk_v_r@mail.ru>
2022-06-07 18:57:48 +0300

    тесты для методов CompanyService

---
+++ src/test/java/com/fedag/internship/service/CompanyServiceTest.java
@@ -0,0 +1,85 @@
+package com.fedag.internship.service;
+
+import com.fedag.internship.domain.entity.CompanyEntity;
+import com.fedag.internship.domain.entity.UserEntity;
+import com.fedag.internship.domain.exception.EntityNotFoundException;
+import com.fedag.internship.domain.mapper.CompanyMapper;
+import com.fedag.internship.repository.CompanyRepository;
+import com.fedag.internship.service.impl.CompanyServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class CompanyServiceTest {
+    @Mock
+    private CompanyMapper companyMapper;
+    @Mock
+    private CompanyRepository companyRepository;
+    @Mock
+    private UserService userService;
+    @InjectMocks
+    private CompanyServiceImpl companyService;
+
+    @Test
+    public void getCompanyByIdTestWhenIdIsInvalid() {
+        Long invalidId = -1L;
+        when(companyRepository.findById(invalidId)).thenReturn(Optional.empty());
+        assertThrows(EntityNotFoundException.class, () -> companyService.getCompanyById(invalidId));
+    }
+
+    @Test
+    public void getCompanyByIdTestWhenIdIsValid() {
+        when(companyRepository.findById(anyLong())).thenReturn(Optional.of(new CompanyEntity()));
+        assertDoesNotThrow(() -> companyService.getCompanyById(anyLong()));
+    }
+
+    @Test
+    public void getAllCompaniesTest() {
+      Pageable pageable = any(Pageable.class);
+      companyService.getAllCompanies(pageable);
+      verify(companyRepository).findAll(pageable);
+    }
+
+    @Test
+    public void deleteCompanyTest() {
+        when(companyRepository.findById(anyLong())).thenReturn(Optional.of(new CompanyEntity()));
+
+        companyService.deleteCompany(anyLong());
+
+        verify(companyRepository).deleteById(anyLong());
+        verify(companyRepository).findById(anyLong());
+    }
+
+    @Test
+    public void createCompanyTest() {
+        UserEntity userWithoutCompany = new UserEntity();
+        CompanyEntity companyWithoutUser = new CompanyEntity();
+        when(userService.getUserById(anyLong())).thenReturn(userWithoutCompany);
+
+        companyService.createCompany(anyLong(), companyWithoutUser);
+
+        assertNotNull(userWithoutCompany.getCompany());
+        assertNotNull(companyWithoutUser.getUser());
+        verify(companyRepository).save(companyWithoutUser);
+    }
+
+    @Test
+    public void updateCompanyTest() {
+        CompanyEntity someCompanyEntity = new CompanyEntity();
+        when(companyRepository.findById(anyLong())).thenReturn(Optional.of(someCompanyEntity));
+
+        companyService.updateCompany(anyLong(), someCompanyEntity);
+
+        verify(companyRepository).save(any());
+        verify(companyMapper).merge(any(), any());
+    }
+}
