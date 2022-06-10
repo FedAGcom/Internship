e67515096038426f1cfca2a460b2713fb7934cf7
Vasiliy <Kiriluk_v_r@mail.ru>
2022-06-10 14:53:06 +0300

    добавил шифрование пароля при создание юзера

--- src/main/java/com/fedag/internship/service/impl/UserServiceImpl.java
+++ src/main/java/com/fedag/internship/service/impl/UserServiceImpl.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserEntity getUserById(Long id) {
@@ -44,6 +46,8 @@ public class UserServiceImpl implements UserService {
     @Transactional
     public UserEntity createUser(UserEntity userEntity) {
         log.info("Создание пользователя");
+        String encodedPassword = passwordEncoder.encode(userEntity.getPassword());
+        userEntity.setPassword(encodedPassword);
         UserEntity result = userRepository.save(userEntity);
         log.info("Пользователь создан");
         return result;
