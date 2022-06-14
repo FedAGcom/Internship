cb1a61a366d742b4bd930b4c26ed79523fad129d
IlyaKayrachka <ilyanur1997@gmail.com>
2022-06-14 15:13:34 +0300

    убрал @EnableScheduling

--- src/main/java/com/fedag/internship/InternshipApplication.java
+++ src/main/java/com/fedag/internship/InternshipApplication.java
@@ -2,10 +2,8 @@ package com.fedag.internship;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class InternshipApplication {
     public static void main(String[] args) {
         SpringApplication.run(InternshipApplication.class, args);
