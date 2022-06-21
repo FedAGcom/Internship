1328fd62aeb83a6990b9d389a5c87b4c226e677e
Damir_Iu <YusupovDamir1490@yandex.ru>
2022-06-21 13:28:45 +0300

    FIX-11_v.3 - Добавлен swagger в RegistrationController

--- src/main/java/com/fedag/internship/controller/RegistrationController.java
+++ src/main/java/com/fedag/internship/controller/RegistrationController.java
@@ -1,9 +1,16 @@
 package com.fedag.internship.controller;
 
+import com.fedag.internship.domain.dto.DtoErrorInfo;
 import com.fedag.internship.domain.dto.request.RegistrationRequest;
 import com.fedag.internship.service.RegistrationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,15 +22,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("registration")
+@Tag(name = "Регистрация", description = "Регистрация пользователей")
 public class RegistrationController {
     private final RegistrationService registrationService;
 
+    @Operation(summary = "Регистрация пользователя")
+    @ApiResponse(responseCode = "201", description = "Пользователи найдены")
+    @ApiResponse(responseCode = "500", description = "Внутренняя ошибка сервера",
+            content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = DtoErrorInfo.class))})
     @PostMapping
     public ResponseEntity<?> register(@RequestBody RegistrationRequest request) {
         registrationService.register(request);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Подтверждение емейла пользователя с помощью токена")
+    @ApiResponse(responseCode = "200", description = "Подтверждение успешно")
+    @ApiResponse(responseCode = "500", description = "Внутренняя ошибка сервера",
+            content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = DtoErrorInfo.class))})
     @GetMapping("/confirm")
     public ResponseEntity<?> confirm(@RequestParam String token) {
         registrationService.confirm(token);
